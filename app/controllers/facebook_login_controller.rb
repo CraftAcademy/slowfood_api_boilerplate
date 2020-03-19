c8079ce07b0836d01b729d69cccaac103e03a901
oliverochman <oliver.ochman@hotmail.com>
2020-03-19 09:42:22 +0100

    remove debuggers

--- app/controllers/facebook_login_controller.rb
+++ app/controllers/facebook_login_controller.rb
@@ -1,13 +1,11 @@
 class FacebookLoginController < DeviseTokenAuth::RegistrationsController
   def create
     if params['provider'] == 'facebook'
-      binding.pry
       user = User.from_omniauth(params.require(:uid), params.require(:email), params.require(:provider))
       if user.persisted?
         sign_in user
         token = user.create_token
         user.save
-        binding.pry
         auth_params = {
           auth_token: token.token,
           client_id: token.client,
